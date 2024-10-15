27689d80f0b52634d0b5eae202be68891700aebf
E. Enkhbold <enkhmunkh@att.net>
2024-10-15 14:13:35 -0500

    Updated the javascript file Display works correctly now When pressed the numbers it updates on the display Still missing the logic for the calculator to wrok

--- calculator.js
+++ calculator.js
@@ -1,3 +1,39 @@
+const skeleton = document.querySelector('.skeleton')
+const display = document.querySelector('.display')
+
+let displayValue = '0';
+
+
+function updateDisplayValue(value) {
+    displayValue = value;
+    display.textContent = value;
+  }
+  
+  document.querySelectorAll('.numbers button').forEach(button => {
+    button.addEventListener('click', () => {
+      const buttonText = button.textContent;
+      
+      if (buttonText === 'C') {
+        displayValue = '0';
+        updateDisplayValue(displayValue);
+      } 
+
+      else if (buttonText === '=') {
+        updateDisplayValue(displayValue);
+      } 
+
+      else {
+        if (displayValue === '0') {
+          displayValue = buttonText;
+        } else {
+          displayValue += buttonText;
+        }
+        updateDisplayValue(displayValue);
+      }
+    });
+  });
+
+
 function add (a , b) {
     return a + b;
 }
